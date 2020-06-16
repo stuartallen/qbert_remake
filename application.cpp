5b0d779a5d050e84109e8d58ede633e6e86fae92
Stuart Allen <stuart.lee.allen@gmail.com>
2020-06-15 19:42:32 -0700

    Created blank window

--- application.cpp
+++ application.cpp
@@ -2,8 +2,68 @@
 //  memory allocation which is
 //  stupid but it works
 #include <iostream>
+#include <time.h>
+#include "board.h"
+
+#include <SDL.h>
+#include <stdio.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 using namespace std;
 
+void guiMainLoop();
+
 int main() {
+    guiMainLoop();
+
     return 0;
+}
+
+void guiMainLoop()
+{
+    int windowSize = 800;
+
+    SDL_Window *window = SDL_CreateWindow("Demo Game",
+                                      SDL_WINDOWPOS_UNDEFINED,
+                                      SDL_WINDOWPOS_UNDEFINED,
+                                      windowSize, windowSize,
+                                      SDL_WINDOW_OPENGL);
+    if (window == nullptr) {
+        SDL_Log("Could not create a window: %s", SDL_GetError());
+        return;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        SDL_Log("Could not create a renderer: %s", SDL_GetError());
+        return;
+    }
+
+    bool got_quit_event = false;
+    while (!got_quit_event) {
+        SDL_Event event;
+        if (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT:
+                got_quit_event = true;
+                break;
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym) {
+                case SDLK_ESCAPE:
+                    got_quit_event = true;
+                    break;
+                }
+                break;
+            }
+        }
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
 }
