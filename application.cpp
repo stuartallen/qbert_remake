cbfdba2ce17cd46c271037e377cbfc25cc7a8d27
Stuart Allen <stuart.lee.allen@gmail.com>
2020-06-16 14:21:29 -0700

    Split up routine that presents to window

--- application.cpp
+++ application.cpp
@@ -14,6 +14,9 @@ const int SCREEN_HEIGHT = 480;
 
 using namespace std;
 
+SDL_Window* set_up_window();
+SDL_Renderer* set_up_renderer(SDL_Window*);
+void destroy_window_renderer(SDL_Window*, SDL_Renderer*);
 void guiMainLoop();
 
 int main(int argc, char* argv[]) {
@@ -24,30 +27,8 @@ int main(int argc, char* argv[]) {
 
 /*  creates screen  */
 void guiMainLoop()  {
-    int windowSizeX = 1200;
-    int windowSizeY = 800;
-    
-    SDL_Window *window = SDL_CreateWindow("Qbert Screen",
-                                      SDL_WINDOWPOS_UNDEFINED,
-                                      SDL_WINDOWPOS_UNDEFINED,
-                                      windowSizeX, windowSizeY,
-                                      SDL_WINDOW_OPENGL);
-    if (window == nullptr) {
-        SDL_Log("Could not create a window: %s", SDL_GetError());
-        return;
-    }
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == nullptr) {
-        SDL_Log("Could not create a renderer: %s", SDL_GetError());
-        return;
-    }
-
-    /* Select the color for drawing. It is set to red here. */
-    SDL_SetRenderDrawColor(renderer, 250, 150, 0, 255);
-
-    /* Clear the entire screen to our selected color. */
-    SDL_RenderClear(renderer);
+    SDL_Window* window = set_up_window();
+    SDL_Renderer* renderer = set_up_renderer(window);
 
     bool got_quit_event = false;
     while (!got_quit_event) {
@@ -66,11 +47,52 @@ void guiMainLoop()  {
                 break;
             }
         }
+
+        SDL_RenderClear(renderer);
+
+        SDL_Rect rect;
+        rect.x = 250;
+        rect.y = 150;
+        rect.w = 200;
+        rect.h = 200;
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderDrawRect(renderer, &rect);
+
+        /*  Draw background and update screen   */
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderPresent(renderer);
     }
+    destroy_window_renderer(window, renderer);
+}
 
+/*  creates window object for the game  */
+SDL_Window* set_up_window() {
+    const int windowSizeX = 1200;
+    const int windowSizeY = 800;
+    SDL_Window *window = SDL_CreateWindow("Qbert Screen",
+                                      SDL_WINDOWPOS_UNDEFINED,
+                                      SDL_WINDOWPOS_UNDEFINED,
+                                      windowSizeX, windowSizeY,
+                                      SDL_WINDOW_OPENGL);
+    if (window == nullptr) {
+        SDL_Log("Could not create a window: %s", SDL_GetError());
+    }
+    return window;
+}
+
+/*  creates renderer object for game    */
+SDL_Renderer* set_up_renderer(SDL_Window* window) {
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        SDL_Log("Could not create a renderer: %s", SDL_GetError());
+    }
+    return renderer;
+}
+
+/*  Handles the memory from renderer and window and quits SDL   */
+void destroy_window_renderer(SDL_Window* window, SDL_Renderer* renderer) {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
 }
